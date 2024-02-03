1ee2489654dd0aefc7ba35ed257de3251e4520f1
Django <django@inventobot.com>
2024-02-03 18:49:13 -0500

    working rfid

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "msp.h"
 #include "driverlib.h"
 
-/**
- * main.c
- */
-void main(void)
-{
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-}
+// /**
+//  * main.c
+//  */
+// void main(void)
+// {
+// 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+// }
